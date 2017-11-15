b05a4371ef10ef0b84698d6b1bb1ef99405b8983
Lou <luana.martelli@heig-vd.ch>
2017-11-15 10:39:26 +0100

    Add bootstrap

---
+++ docs/newspaper.js
@@ -0,0 +1,15 @@
+$.get('./Database/DB_2/20130401.csv', 'utf8', function (data) {
+	var dsv = d3.dsvFormat(';');
+	var allData = dsv.parse(data, (d) => {	
+		var crossfilter = crossfilter(allData);
+		
+		var byActor1 = crossfilter.dimension(function(p) { return p.actor1; });
+		var groupByActor = byActor1.group();
+		groupByActor.top(Infinity).forEach(function(a, i) {
+			console.log(a.key + ": " + a.value);
+		});
+	});
+	
+});
+
+
